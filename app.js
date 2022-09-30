434a40f858881ffe21ceffc83d4bda0075e5da84
Sameer727873 <111059349+Sameer727873@users.noreply.github.com>
2022-09-30 04:55:19 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+function circle(){
+    var cr = document.getElementById("shape")
+    cr.className = "circlebtn"
+     
+}
+
+function square(){
+    var cr = document.getElementById("shape")
+    cr.className = "squarebtn"
+     
+}
+
+function tringle(){
+    var cr = document.getElementById("shape")
+    cr.className = "tringlebtn"
+     
+}
